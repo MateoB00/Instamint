c7bd53a408c3b8e6f8364a9906a293cd4f5b8f23
MateoB00 <mateobianco91100@gmail.com>
2024-03-02 17:45:43 +0100

    feature/create front-end script and the condition for both scripts

--- front/deploy_vercel.sh
+++ front/deploy_vercel.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 CHANGED_FILES=$(git diff --name-only HEAD^ HEAD -- ./)
-echo "Changed files in back directory: $CHANGED_FILES"
+echo "Changed files in front directory: $CHANGED_FILES"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "staging" &&  -n "$CHANGED_FILES" ]] ; then
   # Proceed with the build
