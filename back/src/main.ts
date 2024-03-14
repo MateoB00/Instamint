a8661ee1870acabd83a77e666dca6d393a5f4dc9
MateoB00 <mateobianco91100@gmail.com>
2024-03-14 07:43:42 +0100

    feature/front_create register form with call api, set validator with yup, create and fix files scss and enable cors in api side

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,10 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONT_END_URL,
+  });
   await app.listen(3000);
 }
 bootstrap();
---
+++ front/src/components/ui/Input.tsx
@@ -0,0 +1,40 @@
+import { ChangeEvent, FC } from 'react';
+import '../../scss/components/ui/input.scss';
+
+interface InputProps {
+  type: 'text' | 'number' | 'email' | 'password';
+  label: string;
+  value: string | number;
+  name: string;
+  placeholder: string;
+  disabled?: boolean;
+  onChange?: ChangeEvent<HTMLInputElement>;
+}
+
+const Input: FC<InputProps> = ({
+  type,
+  label,
+  value,
+  name,
+  placeholder,
+  disabled,
+  onChange,
+}) => (
+  <>
+    <label htmlFor={label} className="labelForm">
+      {label}
+    </label>
+    <input
+      className="inputForm"
+      type={type}
+      id={label}
+      value={value}
+      name={name}
+      placeholder={placeholder}
+      disabled={disabled}
+      onChange={onChange}
+    />
+  </>
+);
+
+export default Input;
