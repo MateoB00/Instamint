de40de5f63df37f1cf6a2b63f4b409fe4c0c7a02
LePariisien <alexandrecano@hotmail.fr>
2024-03-17 23:47:41 +0100

    bugfix/resolve conflict with loginForm

--- front/src/components/ui/authForms/loginForm.tsx
+++ front/src/components/ui/authForms/loginForm.tsx
@@ -1,11 +1,50 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import '../../../scss/components/ui/authForms/authForms.scss';
 import '../../../scss/components/ui/authForms/authFormsResponsive.scss';
 import logo from '../../../assets/Image/logo-instamint.svg';
 import Input from '../../../components/ui/Input';
-import { resendEmailConfirmation } from '../../../api/auth';
-import { useLoginForm } from '../../../hooks/useLoginForm';
+import { authLogin, resendEmailConfirmation } from '../../../api/auth';
 import { t } from 'i18next';
+import { shemaLogin, catchErrors } from '../../../utils/yup';
+
+const useLoginForm = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [formErrors, setFormErrors] = useState({
+    email: '',
+    password: '',
+    apiError: '',
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setFormErrors({ email: '', password: '', apiError: '' });
+
+    shemaLogin
+      .validate(formData, { abortEarly: false })
+      .then(async () => {
+        const response = await authLogin(formData.email, formData.password);
+
+        if (response.error) {
+          setFormErrors({
+            email: '',
+            password: '',
+            apiError: response.message,
+          });
+        }
+      })
+      .catch((errors) => {
+        setFormErrors(catchErrors(errors));
+      });
+  };
+
+  return { formData, formErrors, handleChange, handleSubmit };
+};
 
 const fieldsForm = [
   {
@@ -23,7 +62,7 @@ const fieldsForm = [
 ];
 
 export default function LoginForm() {
-  const { formData, formMessages, handleChange, handleSubmit } = useLoginForm();
+  const { formData, formErrors, handleChange, handleSubmit } = useLoginForm();
 
   return (
     <div className="authForm">
@@ -41,19 +80,19 @@ export default function LoginForm() {
               value={formData[field.name as keyof typeof formData]}
               onChange={handleChange}
             />
-            {formMessages[field.name as keyof typeof formMessages] && (
+            {formErrors[field.name as keyof typeof formErrors] && (
               <span style={{ color: 'red' }}>
-                {formMessages[field.name as keyof typeof formMessages]}
+                {formErrors[field.name as keyof typeof formErrors]}
               </span>
             )}
           </Fragment>
         ))}
         <div className="buttonsForm">
           <button className="nextButton">{t('button.login')}</button>
-          {formMessages.apiError && (
-            <span style={{ color: 'red' }}>{formMessages.apiError}</span>
+          {formErrors.apiError && (
+            <span style={{ color: 'red' }}>{formErrors.apiError}</span>
           )}
-          {formMessages.apiError === 'Email not verified' && (
+          {formErrors.apiError === 'Email not verified' && (
             <button onClick={() => resendEmailConfirmation(formData.email)}>
               {t('sendEmailAgain')}
             </button>
