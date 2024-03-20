e816998436450fb6d854dd2c5be8565339e2b0ce
LePariisien <alexandrecano@hotmail.fr>
2024-03-20 19:02:51 +0100

    refactor/change-user-picture optimisation the Profile Page by creating a new file with constants

--- front/src/pages/profile/PageProfile.tsx
+++ front/src/pages/profile/PageProfile.tsx
@@ -1,69 +1,34 @@
-/* eslint-disable max-lines-per-function */
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../../scss/pages/profile/PageProfile.scss';
 import EditPicture from '../../components/ui/profile/EditPicture';
 import { useTranslation } from 'react-i18next';
-import { authChangePicture, getUserData } from '../../api/auth';
+import { getUserData } from '../../api/auth';
 import { Link } from 'react-router-dom';
+import { usePageProfile } from '../../hooks/usePageProfile';
 
-const PageProfile: React.FC = () => {
-  const initialProfilePicture = '../src/assets/Image/user.jpg';
-  const [profilePicture, setProfilePicture] = useState(initialProfilePicture);
-  const [originalProfilePicture, setOriginalProfilePicture] = useState(
-    initialProfilePicture,
-  );
-  const [formErrors, setFormErrors] = useState('');
+export default function PageProfile() {
+  const {
+    handleProfilePictureChange,
+    formErrors,
+    setFormErrors,
+    handleRestoreOriginal,
+    handleSaveClick,
+  } = usePageProfile();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [userData, setUserData] = useState<any>(null);
-
-  const handleProfilePictureChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const dataURL = e.target?.result as string;
-        setProfilePicture(dataURL);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleRestoreOriginal = () => {
-    setProfilePicture(originalProfilePicture);
-  };
-
-  const handleSaveClick = async () => {
-    const formData = new FormData();
-    formData.append('profilePicture', profilePicture);
-
-    try {
-      const response = await authChangePicture(profilePicture);
-
-      if (response.success) {
-        setOriginalProfilePicture(profilePicture);
-      } else {
-        setFormErrors(response.message);
-      }
-    } catch (error) {
-      setFormErrors('An error occurred while uploading the profile picture.');
-    }
-  };
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        // eslint-disable-next-line no-shadow
-        const userData = await getUserData();
-        setUserData(userData);
+        const response = await getUserData();
+        setUserData(response);
       } catch (error) {
         setFormErrors('An error occurred while fetching user data.');
       }
     };
 
     fetchUserData();
-  }, []);
+  }, [setFormErrors]);
 
   const { t } = useTranslation();
 
@@ -90,6 +55,4 @@ const PageProfile: React.FC = () => {
       <Link to="/">return to home</Link>
     </div>
   );
-};
-
-export default PageProfile;
+}
