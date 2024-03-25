4a88953b9e77fa427477f602b1dd31d9595977d0
MateoB00 <mateobianco91100@gmail.com>
2024-03-25 19:05:52 +0100

    feature/create interface for user Data and hooks for User Profile page

---
+++ front/src/hooks/useUserProfile.tsx
@@ -0,0 +1,50 @@
+import { getMe } from '../api/user';
+import { useState } from 'react';
+import { UserInterface } from '../interfaces/userData';
+
+interface LocationState {
+  state: {
+    setOptionsProfiles: 'NFTs' | 'Drafts' | 'Informations';
+  };
+}
+
+export const useUserProfile = () => {
+  const [optionsProfiles, setOptionsProfiles] = useState<
+    'NFTs' | 'Drafts' | 'Informations'
+  >('NFTs');
+
+  const [userData, setUserData] = useState<UserInterface | null>();
+
+  const fetchUserData = async () => {
+    const responseGetMe = await getMe();
+    if (responseGetMe === 401) {
+      setUserData(null);
+    } else {
+      setUserData(responseGetMe);
+    }
+  };
+
+  const handleShowMainProfile = () => {
+    setOptionsProfiles('NFTs');
+  };
+
+  const handleShowUpdateProfile = () => {
+    setOptionsProfiles('Informations');
+  };
+
+  const navigateUserPage = (location: LocationState) => {
+    const initialState = location.state || {};
+    if (initialState.setOptionsProfiles) {
+      setOptionsProfiles(initialState.setOptionsProfiles);
+    }
+  };
+
+  return {
+    optionsProfiles,
+    userData,
+    fetchUserData,
+    handleShowMainProfile,
+    handleShowUpdateProfile,
+    navigateUserPage,
+  };
+};
