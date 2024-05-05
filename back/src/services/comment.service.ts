3d4c7c16712a49e17ebdb876df84227a33162644
soumaya <soumayabenhamadi19@gmail.com>
2024-05-05 22:06:31 +0200

    feature/comments

--- back/src/services/comment.service.ts
+++ back/src/services/comment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Comment } from 'src/entities/comment.entity';
+import { Comment } from '../entities/comment.entity';
 
 @Injectable()
 export class CommentService {
