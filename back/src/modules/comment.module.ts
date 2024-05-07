013994041ba90f42a8f96391e37a709357adb062
soumaya <soumayabenhamadi19@gmail.com>
2024-05-07 22:44:44 +0200

    feature/load_more_comments

--- back/src/modules/comment.module.ts
+++ back/src/modules/comment.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentController } from '../controllers/comment.controller';
 import { CommentService } from '../services/comment.service';
+import { Comment } from 'src/entities/comment.entity';
+import { FirebaseService } from 'src/services/firebase.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment])],
   controllers: [CommentController],
-  providers: [CommentService],
-  exports: [CommentService],
+  providers: [CommentService, FirebaseService],
+  exports: [CommentService, FirebaseService],
 })
 export class CommentModule {}
